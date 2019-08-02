696bce2ca7279c5b5edc387fbc90c4e57ef7e1dc
kishu1998 <kishorsanjaypatil1998@gmail.com>
2019-08-02 11:00:14 +0530

    Updates

---
+++ precache-manifest.3f7cf0137413af9b198185fb67da9a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c093c72fc7810b30ae7464db30e852",
+    "url": "/tic_tac/index.html"
+  },
+  {
+    "revision": "4280d69f70c58a334650",
+    "url": "/tic_tac/static/css/main.ebc048e4.chunk.css"
+  },
+  {
+    "revision": "bcec0b8ff9cb3ef3e44a",
+    "url": "/tic_tac/static/js/2.72817112.chunk.js"
+  },
+  {
+    "revision": "4280d69f70c58a334650",
+    "url": "/tic_tac/static/js/main.5df73ead.chunk.js"
+  },
+  {
+    "revision": "c67d4272b6ecae904420",
+    "url": "/tic_tac/static/js/runtime~main.0cd0b4d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic_tac/static/media/logo.5d5d9eef.svg"
+  }
+]);
